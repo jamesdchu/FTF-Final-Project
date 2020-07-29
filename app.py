7818cc5f4310214f36625ac5bb94feb4e2793dca
aaronam02 <amankwaah123@gmail.com>
2020-07-29 16:14:44 +0000

    profile bug

--- app.py
+++ app.py
@@ -238,7 +238,7 @@ def addUpdate():
     else:
         return "You are not Signed In, navigate to the <a href ='/welcomePage'> welcome page </a> to log in!"
 
-@app.route('communityPage')
+@app.route('/communityPage')
 def communityPage():
     users = mongo.db.user_info
     return render_template('communityPage.html', users=users)
